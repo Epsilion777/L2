c2e82db3c4182a4b8a807c4dff3cb93d8394cc16
Dmitriy <20-sds-01@mail.ru>
2023-09-06 10:11:57 +0300

    main

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "L2/pattern"
+
+func main() {
+	pattern.FacadeFunc()
+	// pattern.BuilderFunc()
+	// pattern.VisitorFunc()
+	// pattern.CommandFunc()
+	// pattern.ChainOfRespFunc()
+	// pattern.FactoryMethodFunc()
+	// pattern.StrategyFunc()
+	// pattern.StateFunc()
+}
